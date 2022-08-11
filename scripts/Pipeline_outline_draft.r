604e56c65381b4a1d3fb6933885ebeeb0e754fb5
RoesM <markdroes@gmail.com>
2022-08-11 16:19:58 -0700

    pipeline progress

--- scripts/Pipeline_outline_draft.r
+++ scripts/Pipeline_outline_draft.r
@@ -24,7 +24,7 @@ lapply(packages, require, character.only=TRUE)
 #-----------------------------
 # load required functions
 #-----------------------------
-
+source("R/compress.r")
 
 #-----------------------------
 # load data inputs
@@ -41,7 +41,7 @@ recap = ldply(list.files('input/mark', pattern = '*recapture_detail.*\\.csv', fu
 obs_bl = ldply(list.files('input/observation', pattern = '^0LL_tag.*\\.csv', full.names = T), read_csv)
 
 #Ptagis-obtained observations
-obs_pt = ldply(list.files('input/observation', pattern = '^TagHist.*\\.csv', full.names = T), read_csv)
+obs_pt = ldply(list.files('input/observation', pattern = '^Lemhi_TagHist.*\\.csv', full.names = T), read_csv)
 
 #list of test tags
 test_tags = ldply(list.files('input/mark', pattern = '^test_tags.*\\.csv', full.names = T), read_csv)
@@ -80,13 +80,26 @@ mark_all = recap %>%
   
   
 #-----------------------------
-# Clean obs_bl data
+# Clean and join observation data
 #-----------------------------
-obs_bl = obs_bl %<>%
-  rename(`Tag Code` = tag,
+obs_pt = obs_pt %<>%
+  janitor::clean_names() %>%
+  mutate(tag_type = "Fish") %>%
+  select(-tag_type)
+
+obs_bl_test = obs_bl %>%
+  filter(tag %in% obs_pt$tag_code) %>% 
+  left_join(meta %>%
+              select(reader,site_code, site_type, array_type), by = c("reader")) %>%
+  rename(tag_code = tag,
          `Event Date Time Value` = detected,
          `Event Site Code Value` = reader,
-         `Antenna ID` = antenna) %>%
+         `Antenna ID` = antenna,
+        ) %>%
+  mutate(`CTH Count` = 1) %>%
+  left_join(obs_pt %>%
+              select(tag_code, mark_species_name, mark_rear_type_name),
+            by = c("tag_code"))
   # you'll need to add this column
   mutate(`Event Type Name` = "Observation") %>%
   janitor::clean_names() %>%
@@ -94,6 +107,8 @@ obs_bl = obs_bl %<>%
   mutate(tag_type = ifelse(tag_code %in% test_tags$test_tags,
                            "Test_Tag",
                            "Fish"))
+
+obs_all =
 
 #-----------------------------
 # Join site info
