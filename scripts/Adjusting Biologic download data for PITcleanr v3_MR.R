3deaff718f2c77c1d0a2ea77a4e5227956aaff24
RoesM <markdroes@gmail.com>
2022-08-11 11:11:55 -0700

    Building pipeline draft

---
+++ scripts/Adjusting Biologic download data for PITcleanr v3_MR.R
@@ -0,0 +1,166 @@
+# Author: Nick Porter
+# Purpose: Create a interface to allow for PITcleanr to be used with Biomark data
+# Created: 10/27/21
+# Last Modified: 8/2/22 by Mark Roes
+
+#-----------------------------------------------------------------
+
+rm(list = ls())
+# load a couple packages
+
+#to install PITcleanr (if you don't have it)
+#remotes::install_github("KevinSee/PITcleanr")
+require(tidyverse)
+require(lubridate)
+require(esquisse)
+library(janitor)
+library(PITcleanr)
+library(readr)
+
+#-----------------------------------------------------------------
+test_tags = ldply(list.files('input/mark', pattern = '^test_tags.*\\.csv', full.names = T), read_csv)
+# read in the  data, and rename some columns
+obs_bl = ldply(list.files('input/observation', pattern = '^0LL_tag.*\\.csv', full.names = T), read_csv) %>%
+  rename(`Tag Code` = tag,
+         `Event Date Time Value` = detected,
+         `Event Site Code Value` = reader,
+         `Antenna ID` = antenna) %>%
+  # you'll need to add this column
+  mutate(`Event Type Name` = "Observation") %>%
+  janitor::clean_names() %>%
+  mutate(antenna_group_configuration_value = 1) %>% #This list is for test tags and defines each tag as Test_Tag or Fish
+  mutate(tag_type = ifelse(tag_code %in% test_tags$test_tags,
+                           "Test_Tag",
+                           "Fish"))
+
+glimpse(obs_bl)
+
+tag_list = obs_bl %>%
+ distinct(tag_code)
+
+#setwd("Merck Sharp & Dohme, Corp/Biomark Radio Telemetry - Documents/General/PCSRF/Data")
+
+#write_csv(tag_list, "unique_tag_list.csv")
+
+tags_per_site = obs_bl %>%
+  group_by(event_site_code_value) %>%
+  distinct(tag_code) %>%
+  count(tag_code) %>%
+  summarise(sum(n))
+
+Tagging_Detail_LITZ <- read_csv("Library/CloudStorage/OneDrive-SharedLibraries-MerckSharp&Dohme,Corp/Biomark Radio Telemetry - Documents/General/PCSRF/Data/Lemhi_Marks/Tagging_Detail_Lemhi_1.1.2020_7.12.2022.csv") %>%
+  janitor::clean_names()
+
+Tagging_Detail_Join = Tagging_Detail_LITZ #%>%
+  #select(tag_code,mark_site_name,mark_date_mmddyyyy, species_name, run_name, length_mm, weight_g, capture_method_code)
+
+Full_DF = obs_bl %>%
+  left_join(Tagging_Detail_Join, by = "tag_code")
+
+Summary_distinct_tags = Full_DF%>%
+  group_by(species_name) %>%
+  #summarise(min(length_mm),max(length_mm),mean(length_mm))
+  distinct(tag_code) %>%
+  count(tag_code) %>%
+  summarise(sum(n))
+
+working_data = Full_DF %>%
+  filter(tag_type == "Fish")
+
+# create a configuration file
+# This sets up your grouping of antennas into an array e.g. upstream which is made up of 3 antennas spanning the river
+config = working_data %>%
+  # first, for example, '16' '17' '18' are re-coded into a single node 'Upstream Array'
+  mutate(node = ifelse(event_site_code_value %in% c('16','17'),
+                       'SSR Entrance',NA),
+            node = ifelse(event_site_code_value %in% c('18'),
+                'SSR Exit', node),
+            # so on with these 4
+            node = ifelse(event_site_code_value %in% c('01','02','03','04'),
+                       'Large HRSC', node),
+            node = ifelse(event_site_code_value %in% c('05','06','07','08','09','10','11','12'),
+                       'SSC Entrance', node),
+            node = ifelse(event_site_code_value %in% c('13','14','15'),
+                'SSC Exit', node)) %>%
+    # need a column called "config_id"
+  mutate(config_id = 1) %>%
+  select(site_code = event_site_code_value,
+         antenna_id,
+         node,
+         config_id) %>%
+  distinct()
+########################
+
+# compress the data
+comp_df = compress(working_data,ignore_event_vs_release = T, configuration = config, units = "secs") #%>%
+
+comp_df$duration <- str_replace_all(comp_df$duration, "secs", '')#removes "secs" from duration
+comp_df$travel_time <- str_replace_all(comp_df$travel_time, "secs", '')#removes "secs" from Travel Time
+
+comp_df = comp_df %>%
+  mutate(duration_sec = as.double(duration)) %>%
+  mutate(travel_time_sec = as.double(travel_time)) %>%
+  left_join(Tagging_Detail_Join, by = "tag_code") 
+
+comp_df_filtered = comp_df %>%
+  select(tag_code,species_name,node,slot,event_type_name,n_dets,min_det,max_det,duration_sec,travel_time_sec,length_mm,weight_g,capture_method_code,mark_site_name,mark_date_mmddyyyy, release_date_mmddyyyy)
+
+residency_df = comp_df_filtered %>%
+  mutate(residency_hr = travel_time_sec/3600) %>%
+  select(tag_code,species_name,node,slot,event_type_name,n_dets,min_det,max_det,duration_sec,travel_time_sec,residency_hr,length_mm,weight_g,capture_method_code,mark_site_name,mark_date_mmddyyyy, release_date_mmddyyyy)
+
+
+
+
+
+
+
+#----------------------------------------------
+# for directionality, you'll need a parent-child table
+# fish move from parent to child
+# you can hand-code this however you want
+parent_child <- tibble(parent = c("Upstream Array",
+                                  "Middle Array",
+                                  "Downstream Array")) %>%
+  mutate(child = lead(parent)) %>%
+  filter(!is.na(child))
+
+buildNodeOrder(parent_child)
+
+# this adds direction to each observation (forward/backward/etc.)
+direct_df = addDirection(comp_df,
+                         parent_child)
+direct_df
+
+tabyl(direct_df,
+      direction)
+
+View(parent_child)
+
+tags_per_site = working_data %>%
+  group_by(antenna) %>%
+  distinct(tag) %>%
+  count(tag) %>%
+  summarise(sum(n))
+
+Overall_distinct_tags = Clean_Litz %>%
+  #filter(!antenna == "HRSC1") %>%
+  #filter(!antenna == "HRSC1.1") %>%
+  distinct(tag)
+
+
+setwd("Merck Sharp & Dohme, Corp/Biomark Radio Telemetry - Documents/General/PCSRF/Data/Biologic Downloads")
+
+write.csv(Overall_distinct_tags, "Litz_tags.csv")
+
+Summary_distinct_tags = Overall_distinct_tags%>%
+  count(tag) %>%
+  summarise(sum(n))
+
+#esquisser(tags_per_site)
+
+investigation = Clean_Litz %>%
+  filter(!antenna == "HRSC1") %>%
+  filter(!antenna == "HRSC1.1") %>%
+  distinct() %>%
+  arrange(tag,date,antenna)
--- scripts/Pipeline_outline_draft.r
+++ scripts/Pipeline_outline_draft.r
@@ -15,7 +15,7 @@ rm(list = ls())
 #-----------------------------
 #Code below automatically loads and/or installs required packages from CRAN
 options(repos=structure(c(cran="https://ftp.osuosl.org/pub/cran/")))  
-packages <- c("plyr","tidyverse","readxl", "lubridate") 
+packages <- c("plyr","tidyverse","readxl", "lubridate","esquisse","readr","janitor") 
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   install.packages(setdiff(packages, rownames(installed.packages())))  
 }  
@@ -37,21 +37,24 @@ recap = ldply(list.files('input/mark', pattern = '*recapture_detail.*\\.csv', fu
   mutate(`Mark Date MMDDYYYY` = mdy(`Mark Date MMDDYYYY`))
 
 #Observation data
-#biomark-obtained observations
-obs_bm = ldply(list.files('input/observation', pattern = '^0LL_tag.*\\.csv', full.names = T), read_csv)
+#biologis-obtained observations
+obs_bl = ldply(list.files('input/observation', pattern = '^0LL_tag.*\\.csv', full.names = T), read_csv)
 
 #Ptagis-obtained observations
-obs_pt = ldply(list.files('input/observation', pattern = '*tag_history.*\\.csv', full.names = T), read_csv)
+obs_pt = ldply(list.files('input/observation', pattern = '^TagHist.*\\.csv', full.names = T), read_csv)
+
+#list of test tags
+test_tags = ldply(list.files('input/mark', pattern = '^test_tags.*\\.csv', full.names = T), read_csv)
 
 #read in site data
-locs = ldply(list.files('input/site', pattern = '^Litz_Locations*\\.csv', full.names = T), read_csv)
-
 meta = ldply(list.files('input/site', pattern = '^site_metadata.*\\.xlsx', full.names = T), read_excel)
 
 
 #-----------------------------
 # Clean mark data
 #-----------------------------
+
+#CODE CHUNK BELOW NOT NECESSARY FOR PTAGIS-FRONTLOADED PROCESS ---- 
 #Mark data is all unique tag codes from the mark df, along with 'recap' tag codes that are not present in the mark df. These were tagged above the RSTs in the mark df
 
 #find # of tag codes in recap but not mark
@@ -59,9 +62,13 @@ length(setdiff(recap$`Tag Code`,mark$`Tag Code`))
 
 dim(filter(recap, `Tag Code` %in% setdiff(recap$`Tag Code`,mark$`Tag Code`) & `Recap Count` ==1))
 
+all_tags = unique(c(mark$`Tag Code`,recap$`Tag Code`))
+write_csv(tibble(taglist), file = 'input/mark/Full_TagList.csv')
+
 #So we want to pull out the first instance of each 'recap' that is actually a new tag record
 
 #filter down the recap data set to the columns and records to join to the mark data
+#This provides the initial tag list and allows us to identify the initial observations of recaps
 mark_all = recap %>%
   filter(`Tag Code` %in% setdiff(recap$`Tag Code`,mark$`Tag Code`)) %>%
   group_by(`Tag Code`) %>%
@@ -73,9 +80,20 @@ mark_all = recap %>%
   
   
 #-----------------------------
-# Join observations
+# Clean obs_bl data
 #-----------------------------
-
+obs_bl = obs_bl %<>%
+  rename(`Tag Code` = tag,
+         `Event Date Time Value` = detected,
+         `Event Site Code Value` = reader,
+         `Antenna ID` = antenna) %>%
+  # you'll need to add this column
+  mutate(`Event Type Name` = "Observation") %>%
+  janitor::clean_names() %>%
+  mutate(antenna_group_configuration_value = 1) %>% #This list is for test tags and defines each tag as Test_Tag or Fish
+  mutate(tag_type = ifelse(tag_code %in% test_tags$test_tags,
+                           "Test_Tag",
+                           "Fish"))
 
 #-----------------------------
 # Join site info
